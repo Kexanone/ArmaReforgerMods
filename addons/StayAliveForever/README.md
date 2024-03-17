8c0ccfb4a5abc58307ed6cb5274d084ac9aa4f21
Kex <kexanone@gmail.com>
2024-03-17 17:06:57 +0100

    Add StayAliveForever

---
+++ addons/StayAliveForever/README.md
@@ -0,0 +1,8 @@
+# Stay Alive Forever
+
+While this mod is active, players can only fall unconscious and never die.
+Use `Stay Alive` instead if you want players always to fall unconscious, but still be killable.
+
+Feedback tracker: https://github.com/Kexanone/ArmaReforgerMods/issues<br>
+Source Code: https://github.com/Kexanone/ArmaReforgerMods/tree/main/addons/StayAliveForever<br>
+Workshop: https://reforger.armaplatform.com/workshop/5EDB1D94A87AA78B
---
+++ addons/StayAliveForever/scripts/Game/SA/Components/Damage/SCR_CharacterDamageManagerComponent.c
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------------------------
+modded class SCR_CharacterDamageManagerComponent : SCR_DamageManagerComponent
+{
+	//-----------------------------------------------------------------------------------------------------------
+	//! Add handlers for protecting players from being killed
+	override void SA_Initialize(IEntity owner)
+	{
+		super.SA_Initialize(owner);
+		m_pBloodHitZone.SA_EnableProtection(true);
+		m_pSA_HealthHitZone.SA_EnableProtection(true);
+	}
+}

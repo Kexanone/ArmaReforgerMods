9814a06b9fd730b74628aae6f01536e760b043a1
Kex <kexanone@gmail.com>
2025-02-27 23:11:40 +0100

    BandagePatch - Update script prefix

--- addons/BandagePatch/Scripts/Game/BP/Components/Gadgets/SCR_ConsumableItemComponent.c
+++ addons/BandagePatch/Scripts/Game/BP/Components/Gadgets/SCR_ConsumableItemComponent.c
@@ -5,14 +5,14 @@ modded class SCR_ConsumableItemComponent : SCR_GadgetComponent
 	override protected void OnApplyToCharacter(IEntity item, bool successful, ItemUseParameters animParams)
 	{
 		if (Replication.IsServer() && SCR_ConsumableBandage.Cast(GetConsumableEffect()))
-			Rpc(RpcDo_FB_ClearInvokersBroadcast, animParams.GetIntParam());
+			Rpc(RpcDo_BP_ClearInvokersBroadcast, animParams.GetIntParam());
 		
 		super.OnApplyToCharacter(item, successful, animParams);
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
-	protected void RpcDo_FB_ClearInvokersBroadcast(int targetGroup)
+	protected void RpcDo_BP_ClearInvokersBroadcast(int targetGroup)
 	{
 		ClearInvokers(targetGroup);
 	}

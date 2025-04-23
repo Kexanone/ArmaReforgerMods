e9a7c12d95fd40f77270cfbc512f3c6142ed64b4
Kex <kexanone@gmail.com>
2025-04-23 03:07:39 +0200

    BandagePatch - Improved implementation

--- addons/BandagePatch/Scripts/Game/BP/Components/Gadgets/SCR_ConsumableItemComponent.c
+++ addons/BandagePatch/Scripts/Game/BP/Components/Gadgets/SCR_ConsumableItemComponent.c
@@ -1,19 +1,39 @@
 //------------------------------------------------------------------------------------------------
 modded class SCR_ConsumableItemComponent : SCR_GadgetComponent
 {
+	protected ChimeraCharacter m_pBP_TargetCharacter;
+	
 	//------------------------------------------------------------------------------------------------
-	override protected void OnApplyToCharacter(IEntity item, bool successful, ItemUseParameters animParams)
+	//! Broadcast the removal of the lock
+	override protected void SetHealedGroup(EBandagingAnimationBodyParts group, bool healed)
 	{
-		if (Replication.IsServer() && SCR_ConsumableBandage.Cast(GetConsumableEffect()))
-			Rpc(RpcDo_BP_ClearInvokersBroadcast, animParams.GetIntParam());
+		if (!Replication.IsServer())
+			return;
 		
-		super.OnApplyToCharacter(item, successful, animParams);
+		if (!m_pBP_TargetCharacter)
+			m_pBP_TargetCharacter = ChimeraCharacter.Cast(GetTargetCharacter());
+		
+		RplId targetCharID = Replication.FindItemId(m_pBP_TargetCharacter);
+		RpcDo_BP_SetHealedGroupBroadcast(targetCharID, group, healed);
+		Rpc(RpcDo_BP_SetHealedGroupBroadcast, targetCharID, group, healed);
 	}
 	
 	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
-	protected void RpcDo_BP_ClearInvokersBroadcast(int targetGroup)
+	protected void RpcDo_BP_SetHealedGroupBroadcast(RplId targetCharID, EBandagingAnimationBodyParts group, bool healed)
 	{
-		ClearInvokers(targetGroup);
+		m_iStoredHealedGroup = group * healed;
+		
+		if (!m_pBP_TargetCharacter)
+			m_pBP_TargetCharacter = ChimeraCharacter.Cast(Replication.FindItem(targetCharID));
+		
+		if (!m_pBP_TargetCharacter)
+			return;
+		
+		SCR_CharacterDamageManagerComponent targetDamageManager = SCR_CharacterDamageManagerComponent.Cast(m_pBP_TargetCharacter.GetDamageManager());
+		if (!targetDamageManager)
+			return;
+		
+		targetDamageManager.SetGroupIsBeingHealed(targetDamageManager.FindAssociatedHitZoneGroup(group), healed);
 	}
 }

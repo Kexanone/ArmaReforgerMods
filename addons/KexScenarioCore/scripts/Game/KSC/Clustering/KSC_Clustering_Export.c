ca4c4b1e14236c1ad2c4cf026cc5e4db7f2264f5
Kex <kexanone@gmail.com>
2025-05-26 22:01:19 +0200

    KexScenarioCore - Update exporter

--- addons/KexScenarioCore/scripts/Game/KSC/Clustering/KSC_Clustering_Export.c
+++ addons/KexScenarioCore/scripts/Game/KSC/Clustering/KSC_Clustering_Export.c
@@ -6,39 +6,73 @@ class KSC_Clustering_Exporter : Managed
 	protected ref map<typename, ref FileHandle> m_mStreams = new map<typename, ref FileHandle>();
 	
 	//------------------------------------------------------------------------------------------------
-	void Run(array<typename> types = null, array<ref array<float>> rectangles = null, bool negatePlacement=false)
+	void Run(array<typename> types = null, array<ref array<float>> rectangles = null)
 	{
 		m_sWorldName = FilePath.StripExtension(FilePath.StripPath(GetGame().GetWorldFile()));
 		FileIO.MakeDirectory(m_sWorldName);
 		ExportObjects(types);
 		
 		if (rectangles)
-			ExportTerrain(rectangles, negatePlacement);
+			ExportTerrain(rectangles);
 		else
-			ExportTerrain(negatePlacement);
+			ExportTerrain();
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	void ExportTerrain(bool negatePlacement=false)
+	void ExportTerrain()
 	{
 		BaseWorld world = GetGame().GetWorld();
 		vector mins, maxs;
 		world.GetBoundBox(mins, maxs);
 		array<ref array<float>> rectangles = {{mins[0], mins[2], maxs[0], maxs[2]}};
-		ExportTerrain(rectangles, negatePlacement);
+		ExportTerrain(rectangles);
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	void ExportTerrain(array<ref array<float>> rectangles, bool negatePlacement=false)
+	void ExportTerrain(array<ref array<float>> rectangles)
 	{
-		array<int> idxListRoad = {}, idxListPlacement = {};
+		map<KSC_Clustering_EExportType, ref FileHandle> m_mStreams = new map<KSC_Clustering_EExportType, ref FileHandle>();
+		m_mStreams[KSC_Clustering_EExportType.ROAD] = FileIO.OpenFile(m_sWorldName + "/" + "Road.bin", FileMode.WRITE);
+		m_mStreams[KSC_Clustering_EExportType.PLACEMENT] = FileIO.OpenFile(m_sWorldName + "/" + "Placement.bin", FileMode.WRITE);
+		
+		int xMin = int.MAX;
+		int zMin = int.MAX;
+		int xMax = int.MIN;
+		int zMax = int.MIN;
+		
+		foreach (array<float> rectangle : rectangles)
+		{
+			int xMinRectangle = Math.Round(rectangle[0]);
+			if (xMinRectangle < xMin)
+				xMin = xMinRectangle;
+			
+			int zMinRectangle = Math.Round(rectangle[1]);
+			if (zMinRectangle < zMin)
+				zMin = zMinRectangle;
+			
+			int xMaxRectangle = Math.Round(rectangle[2]);
+			if (xMaxRectangle > xMax)
+				xMax = xMaxRectangle;
+			
+			int zMaxRectangle = Math.Round(rectangle[3]);
+			if (zMaxRectangle > zMax)
+				zMax = zMaxRectangle;
+		}
+		
+		// Write header
+		foreach (FileHandle stream : m_mStreams)
+		{
+			stream.Write(xMin); stream.Write(xMax - xMin + 1);
+			stream.Write(zMin); stream.Write(zMax - zMin + 1);
+			stream.Write(0);
+		}
 
 		foreach (array<float> rectangle : rectangles)
 		{
-			int xMin = Math.Round(rectangle[0]);
-			int zMin = Math.Round(rectangle[1]);
-			int xMax = Math.Round(rectangle[2]);
-			int zMax = Math.Round(rectangle[3]);
+			xMin = Math.Round(rectangle[0]);
+			zMin = Math.Round(rectangle[1]);
+			xMax = Math.Round(rectangle[2]);
+			zMax = Math.Round(rectangle[3]);
 			
 			for (int x = xMin; x <= xMax; x++)
 			{
@@ -49,149 +83,24 @@ class KSC_Clustering_Exporter : Managed
 					
 					if (KSC_TerrainHelper.SurfaceIsRoad(pos))
 					{
-						idxListRoad.Insert(x);
-						idxListRoad.Insert(z);
+						m_mStreams[KSC_Clustering_EExportType.ROAD].Write(x);
+						m_mStreams[KSC_Clustering_EExportType.ROAD].Write(z);
 					}
 					
-					if (IsPlacement(pos) ^ negatePlacement)
+					if (IsPlacement(pos))
 					{
-						idxListPlacement.Insert(x);
-						idxListPlacement.Insert(z);
+						m_mStreams[KSC_Clustering_EExportType.PLACEMENT].Write(x);
+						m_mStreams[KSC_Clustering_EExportType.PLACEMENT].Write(z);
 					}
 				}
 			}
 		}
 		
-		BaseWorld world = GetGame().GetWorld();
-		vector mins, maxs;
-		world.GetBoundBox(mins, maxs);
-		int xMin = Math.Round(mins[0]);
-		int xMax = Math.Round(maxs[0]);
-		int zMin = Math.Round(mins[2]);
-		int zMax = Math.Round(maxs[2]);
-		
-		PrintFormat("%1 %2 %3 %4 %5", xMin, xMax - xMin + 1, zMin, zMax - zMin + 1, idxListRoad.Count() / 2);
-		FileHandle streamRoad = FileIO.OpenFile(m_sWorldName + "/" + "Road.bin", FileMode.WRITE);
-		streamRoad.Write(xMin); streamRoad.Write(xMax - xMin + 1);
-		streamRoad.Write(zMin); streamRoad.Write(zMax - zMin + 1);
-		streamRoad.Write(idxListRoad.Count() / 2);
-		
-		foreach (int idx : idxListRoad)
+		foreach (FileHandle stream : m_mStreams)
 		{
-			streamRoad.Write(idx);
-		}
-		
-		streamRoad.Close();
-		
-		FileHandle streamPlacement = FileIO.OpenFile(m_sWorldName + "/" + "Placement.bin", FileMode.WRITE);
-		streamPlacement.Write(xMin); streamPlacement.Write(xMax - xMin + 1);
-		streamPlacement.Write(zMin); streamPlacement.Write(zMax - zMin + 1);
-		streamPlacement.Write(idxListPlacement.Count() / 2);
-		
-		foreach (int idx : idxListPlacement)
-		{
-			streamPlacement.Write(idx);
-		}
-		
-		streamPlacement.Close();
-	}
-	
-	static void loadRoadSlots(EEditableEntityLabel label, out array<int> xList, out array<int> zList, out array<float> dirList)
-	{
-		string path = KSC_WorldTools.GetBaseWorldName() + "/";
-		
-		switch (label)
-		{
-			case EEditableEntityLabel.SLOT_ROAD_SMALL: { path += "RoadSmall.bin"; break; }
-			case EEditableEntityLabel.SLOT_ROAD_MEDIUM: { path += "RoadMedium.bin"; break; }
-			case EEditableEntityLabel.SLOT_ROAD_LARGE: { path += "RoadLarge.bin"; break; }
-			default: { return; }
-		}
-		
-		FileHandle stream = FileIO.OpenFile(path, FileMode.READ);
-		stream.Seek(16);
-		int nElements;
-		stream.Read(nElements, 4);
-		Print(nElements);
-		xList.Reserve(nElements);
-		zList.Reserve(nElements);
-		dirList.Reserve(nElements);
-		
-		for (int i = 0; i < nElements; i++)
-		{
-			int x, z;
-			float value;
-			  
-			stream.Read(x, 4); xList.Insert(x);
-			stream.Read(z, 4); zList.Insert(z);
-			stream.Read(value, 4); dirList.Insert(value);
+			stream.Close();
 		}
 	}
-	
-	static void loadFlatSlots(EEditableEntityLabel label, out array<int> xList, out array<int> zList)
-	{
-		string path = KSC_WorldTools.GetBaseWorldName() + "/";
-		
-		switch (label)
-		{
-			case EEditableEntityLabel.SLOT_FLAT_SMALL: { path += "FlatSmall.bin"; break; }
-			case EEditableEntityLabel.SLOT_FLAT_MEDIUM: { path += "FlatMedium.bin"; break; }
-			case EEditableEntityLabel.SLOT_FLAT_LARGE: { path += "FlatLarge.bin"; break; }
-			default: { return; }
-		}
-		
-		FileHandle stream = FileIO.OpenFile(path, FileMode.READ);
-		stream.Seek(16);
-		int nElements;
-		stream.Read(nElements, 4);
-		Print(nElements);
-		xList.Reserve(nElements);
-		zList.Reserve(nElements);
-		
-		for (int i = 0; i < nElements; i++)
-		{
-			int x, z;
-			  
-			stream.Read(x, 4); xList.Insert(x);
-			stream.Read(z, 4); zList.Insert(z);
-		}
-	}
-	
-	/*
-	protected void ExportTerrain(int xStep = 1, int zStep = 1)
-	{
-		BaseWorld world = GetGame().GetWorld();
-		vector mins, maxs;
-		world.GetBoundBox(mins, maxs);
-		int xMin = Math.Round(mins[0]);
-		int xMax = Math.Round(maxs[0]);
-		int zMin = Math.Round(mins[2]);
-		int zMax = Math.Round(maxs[2]);
-		
-		FileHandle streamRoad = FileIO.OpenFile(m_sWorldName + "/" + "Road.dat", FileMode.WRITE);
-		FileHandle streamPlacement = FileIO.OpenFile(m_sWorldName + "/" + "Placement.dat", FileMode.WRITE);
-		streamRoad.Write(string.Format("(%1,%2,%3|%4,%5,%6)\n", xMin, xStep, xMax, zMin, zStep, zMax));
-		streamPlacement.Write(string.Format("(%1,%2,%3|%4,%5,%6)\n", xMin, xStep, xMax, zMin, zStep, zMax));
-		
-		for (int x = xMin; x <= xMax; x += xStep)
-		{
-			for (int z = zMin; z <= zMax; z += zStep)
-			{
-				vector pos = Vector(x, 0, z);
-				pos[1] = SCR_TerrainHelper.GetTerrainY(pos);
-				ResourceName material = KSC_TerrainHelper.GetSurfaceMaterial(pos);
-				streamRoad.Write(KSC_TerrainHelper.SurfaceIsRoad(pos).ToString(true));
-				streamPlacement.Write(IsPlacement(pos).ToString(true));
-			}
-			
-			streamRoad.Write("\n");
-			streamPlacement.Write("\n");
-		}
-		
-		streamRoad.Close();
-		streamPlacement.Close();
-	}
-	*/
 	
 	//------------------------------------------------------------------------------------------------
 	protected bool IsPlacement(vector pos)
@@ -249,3 +158,9 @@ class KSC_Clustering_Exporter : Managed
 	}
 }
 
+//------------------------------------------------------------------------------------------------
+enum KSC_Clustering_EExportType
+{
+	ROAD,
+	PLACEMENT
+}

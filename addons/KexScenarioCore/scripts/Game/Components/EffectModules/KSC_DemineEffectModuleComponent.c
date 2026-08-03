420ecdc45833aaa08d3806a576132ca030e8aa60
Kex <kexanone@gmail.com>
2026-08-03 03:56:08 +0200

    KexScenarioCore - Use wrapper for demining handling and handle destruction of mine

--- addons/KexScenarioCore/scripts/Game/Components/EffectModules/KSC_DemineEffectModuleComponent.c
+++ addons/KexScenarioCore/scripts/Game/Components/EffectModules/KSC_DemineEffectModuleComponent.c
@@ -6,6 +6,7 @@ class KSC_DemineEffectsModuleComponentClass : SCR_EffectsModuleComponentClass
 //------------------------------------------------------------------------------------------------
 class KSC_DemineEffectsModuleComponent : SCR_EffectsModuleComponent
 {
+	protected ref array<ref KSC_DeminingEffectModuleComponent_MineEventsWrapper> m_MineWrappers = {};
 	protected int m_iArmedCount = 0;
 	protected ref ScriptInvoker m_OnAllDemined;
 	
@@ -21,30 +22,18 @@ class KSC_DemineEffectsModuleComponent : SCR_EffectsModuleComponent
 		if (!spawnedOwner)
 			return;
 		
-		SCR_PressureTriggerComponent trigger = SCR_PressureTriggerComponent.Cast(spawnedOwner.FindComponent(SCR_PressureTriggerComponent));
-		if (!trigger)
-			return;
-			
-		trigger.KSC_GetOnDoTrigger().Insert(HandleDemining);
-		trigger.KSC_GetOnDisarmTrigger().Insert(HandleDemining);
+		m_MineWrappers.Insert(new KSC_DeminingEffectModuleComponent_MineEventsWrapper(this, spawnedOwner));
 		m_iArmedCount++;
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	protected void HandleDemining(SCR_PressureTriggerComponent trigger)
+	[Friend(KSC_DeminingEffectModuleComponent_MineEventsWrapper)]
+	protected void HandleDemining(KSC_DeminingEffectModuleComponent_MineEventsWrapper wrapper)
 	{
-		if (trigger)
-		{
-			trigger.KSC_GetOnDoTrigger().Remove(HandleDemining);
-			trigger.KSC_GetOnDisarmTrigger().Remove(HandleDemining);
-		}
-		
+		m_MineWrappers.RemoveItem(wrapper);
 		m_iArmedCount--;
 		
-		if (m_iArmedCount > 0)
-			return;
-		
-		if (m_OnAllDemined)
+		if (m_iArmedCount <= 0 && m_OnAllDemined)
 			m_OnAllDemined.Invoke(GetOwner());
 	}
 	
@@ -64,5 +53,58 @@ class KSC_DemineEffectsModuleComponent : SCR_EffectsModuleComponent
 		
 		if (m_OnAllDemined)
 			m_OnAllDemined.Invoke(owner);
+	}
+}
+
+//------------------------------------------------------------------------------------------------
+//! Events wrapper for mine that calls back parent on demining
+class KSC_DeminingEffectModuleComponent_MineEventsWrapper
+{
+	protected KSC_DemineEffectsModuleComponent m_Parent;
+	protected IEntity m_Mine;
+	
+	//------------------------------------------------------------------------------------------------
+	void KSC_DeminingEffectModuleComponent_MineEventsWrapper(KSC_DemineEffectsModuleComponent parent, IEntity mine)
+	{
+		m_Parent = parent;
+		m_Mine = mine;
+		
+		SCR_PressureTriggerComponent trigger = SCR_PressureTriggerComponent.Cast(mine.FindComponent(SCR_PressureTriggerComponent));
+		if (trigger)
+			trigger.KSC_GetOnDisarmTrigger().Insert(HandleDemining);
+		
+		SCR_MineDamageManager damageManager = SCR_MineDamageManager.Cast(mine.FindComponent(SCR_MineDamageManager));
+		if (damageManager)
+			damageManager.GetOnDamageStateChanged().Insert(HandleDamageStateChanged);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void HandleDemining(SCR_PressureTriggerComponent trigger)
+	{
+		m_Parent.HandleDemining(this);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	protected void HandleDamageStateChanged(EDamageState newState)
+	{
+		if (newState != EDamageState.DESTROYED)
+			return;
+		
+		m_Parent.HandleDemining(this);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void ~KSC_DeminingEffectModuleComponent_MineEventsWrapper()
+	{
+		if (!m_Mine)
+			return;
+		
+		SCR_PressureTriggerComponent trigger = SCR_PressureTriggerComponent.Cast(m_Mine.FindComponent(SCR_PressureTriggerComponent));
+		if (trigger)
+			trigger.KSC_GetOnDisarmTrigger().Insert(HandleDemining);
+		
+		SCR_MineDamageManager damageManager = SCR_MineDamageManager.Cast(m_Mine.FindComponent(SCR_MineDamageManager));
+		if (damageManager)
+			damageManager.GetOnDamageStateChanged().Insert(HandleDamageStateChanged);
 	}
 }

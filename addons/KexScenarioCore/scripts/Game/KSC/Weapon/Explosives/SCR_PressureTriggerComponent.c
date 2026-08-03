ab263008c3d879999c0938ad7f6130aa2932a314
Kex <kexanone@gmail.com>
2026-08-03 04:26:23 +0200

    KexScenarioCore - Revert "Remove unused invoker"
    This reverts commit d8ad3dc2d91eb9ded995fa4f177c58a9c272aea9.

--- addons/KexScenarioCore/scripts/Game/KSC/Weapon/Explosives/SCR_PressureTriggerComponent.c
+++ addons/KexScenarioCore/scripts/Game/KSC/Weapon/Explosives/SCR_PressureTriggerComponent.c
@@ -2,7 +2,18 @@
 //! Add invoker for disarming
 modded class SCR_PressureTriggerComponent : SCR_BaseTriggerComponent
 {
+	protected ref ScriptInvoker m_KSC_OnDoTrigger;
 	protected ref ScriptInvoker m_KSC_OnDisarmTrigger;
+	
+	//------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
+	override void RPC_DoTrigger()
+	{
+		super.RPC_DoTrigger();
+		
+		if (m_KSC_OnDoTrigger)
+			m_KSC_OnDoTrigger.Invoke(this);
+	}
 	
 	//------------------------------------------------------------------------------------------------
 	override void DisarmTrigger()
@@ -11,6 +22,15 @@ modded class SCR_PressureTriggerComponent : SCR_BaseTriggerComponent
 		
 		if (m_KSC_OnDisarmTrigger)
 			m_KSC_OnDisarmTrigger.Invoke(this);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	ScriptInvoker KSC_GetOnDoTrigger()
+	{
+		if (!m_KSC_OnDoTrigger)
+			m_KSC_OnDoTrigger = new ScriptInvoker();
+		
+		return m_KSC_OnDoTrigger;
 	}
 	
 	//------------------------------------------------------------------------------------------------
